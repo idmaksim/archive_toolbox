76b50a92ad5014696d1b7666d22c4744da49aa2b
idmaksim <i@dmaksim.ru>
2024-07-01 02:21:45 +0300

    fix bugs

--- archive_toolbox/logic.py
+++ archive_toolbox/logic.py
@@ -1,11 +1,13 @@
 import os
-import shutil
+import tarfile
+from zipfile import ZipFile
 
 from utils import check_double_point_type
 
 
 def extract_archive(filename: str) -> None:
     archive_name = os.path.splitext(filename)[0]
+    is_zip = os.path.splitext(filename)[1] == ".zip"
 
     if check_double_point_type(filename):
         archive_name = os.path.splitext(archive_name)[0]
@@ -13,5 +15,21 @@ def extract_archive(filename: str) -> None:
     if not os.path.exists(archive_name):
         os.mkdir(archive_name)
 
-    shutil.unpack_archive(filename, archive_name)
+    if is_zip:
+        with ZipFile(filename) as archive:
+            for file in archive.namelist():
+                print(f'[+] Extracting {file} from {filename}')
+                archive.extract(file, archive_name)
+    else:
+        with tarfile.open(filename) as archive:
+            for file in archive.getmembers():
+                print(f'[+] Extracting {file.name} from {filename}')
+                archive.extract(file, archive_name)
+
     print(f"Extracted all files from {filename} to {archive_name}")
+
+
+def create_tar_file(filename: str) -> None:
+    with tarfile.open(filename, 'w') as tar:
+        tar.add(filename)
+    print(f"Created tar file {filename}")

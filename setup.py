49b79ff2874cb1f92527639a0328f733a0538d12
idmaksim <i@dmaksim.ru>
2024-07-01 13:10:05 +0300

    upd setup ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='archive_toolbox',
-    version='0.3',
+    version='1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [

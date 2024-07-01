66734b4f083245208096c2d926d04b501c72b02a
idmaksim <i@dmaksim.ru>
2024-07-01 11:20:56 +0300

    upda setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='archive_toolbox',
-    version='0.1',
+    version='0.3',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'atb = archive_tollbox.main:main',
+            'atb = archive_toolbox.main:main',
         ],
     },
     install_requires=[
